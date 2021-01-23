74846866f1cf96ca63f288ea428dd7fede04a1a2
Eleni Fatsea <efatsea@gmail.com>
2021-01-23 18:44:03 +0200

    add in function creatSore the function subscribe()

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 function createStore () {
   //create a function that creates store objects
 	let state     //the state of the store 
-
+	let listeners = [] //array
   //a function to get the state
 	const getState = () => state
 
-  // when createSore is invoked, it will return an object back that invoke the getState method
+  //function that push the data fron subscribe in the listeners
+	const subscribe = (listener) =>{
+		listeners.push(listener)
+		return() => {
+			listeners=listeners.filter((l) = > l!== listener)
+		}
+	}
+
+  // when createStore is invoked, it will return an object back that invoke the getState method
 	return{
-		getState
+		getState,
+		subscribe
 	}
 }
+
+const store = createStore()
+store.subscribe(()=>{
+	console.log('The new state is:',store.getState())
+})
+
+const unsubscribe = store.subscribe(() =>{
+	console.log('The store changed')
+})
