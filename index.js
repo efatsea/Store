12ee57aab938db2d7aa17cb09cf0e2fce4673ce0
Eleni Fatsea <efatsea@gmail.com>
2021-01-27 19:16:28 +0200

    change the text action to be a variable

--- index.js
+++ index.js
@@ -34,15 +34,21 @@ function createStore(reducer) {
 
 //App Code
 
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
 //reduce fuction and also a pure function, takes as arguments the current 
 //state and the action and return the state.
 function todos(state=[],action){
 	switch(action.type) {
-		case 'ADD_TODO':
+		case ADD_TODO:
 			return state.concat([action.todo])
-		case 'REMOVE_TODO' :
+		case REMOVE_TODO :
 			return state.filter((todo)=>todo.id !== action.id)
-		case 'TOGGLE_TODO' :
+		case TOGGLE_TODO :
 			return state.map((todo)=> todo.id !==action.id ? todo :
 			Object.assign({}, todo, { complete: !todo.complete}))
 		default :
@@ -53,9 +59,9 @@ function todos(state=[],action){
 
 function goals(state=[],action){
 	switch(action.type){
-		case 'ADD_GOAL' :
+		case ADD_GOAL :
 			return state.concat([action.goal])
-		case 'REMOVE_GOAL' :
+		case REMOVE_GOAL :
 			return state.filter((goal)=>goal.id !== action.id)
 		default:
 			return state
@@ -78,7 +84,7 @@ store.subscribe(()=>{
 
 
 store.dispatch({
-	type: 'ADD_TODO',
+	type: ADD_TODO,
 	todo:{
 		id:0,
 		name:'Learn Redux',
@@ -86,7 +92,7 @@ store.dispatch({
 	}
 })
 store.dispatch({
-	type: 'ADD_TODO',
+	type: ADD_TODO,
 	todo:{
 		id:1,
 		name:'Learn HTML',
@@ -94,20 +100,20 @@ store.dispatch({
 	}
 })
 store.dispatch({
-	type: 'REMOVE_TODO',
+	type: REMOVE_TODO,
 	id:1
 
 	
 })
 store.dispatch({
-	type: 'ADD_GOAL',
+	type: ADD_GOAL,
 	goal:{
 		id:0,
 		name:'Learn Redux',
 	}
 })
 store.dispatch({
-	type: 'ADD_GOAL',
+	type: ADD_GOAL,
 	goal:{
 		id:1,
 		name:'Learn general',
@@ -115,7 +121,7 @@ store.dispatch({
 	}
 })
 store.dispatch({
-	type: 'TOGGLE_TODO',
+	type: TOGGLE_TODO,
 	id:0
 })
 
